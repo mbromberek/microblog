b96571c3ad33f0099b6719709679612126a26f6f
Mike Bromberek <mikebromberek@gmail.com>
2021-02-14 18:45:55 -0600

    User can login with username and password stored in users database table

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
 import flask
+from flask import request
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -16,13 +21,27 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    site_code = flask.render_template('index.html', title='Home', user=user, posts=posts)
+    site_code = flask.render_template('index.html', title='Home', posts=posts)
     return site_code
 
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return flask.redirect(flask.url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flask.flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return flask.redirect(flask.url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flask.flash('Invalid username or password')
+            return flask.redirect(flask.url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = flask.url_for('index')
+        return flask.redirect(next_page)
     return flask.render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return flask.redirect(flask.url_for('index'))
