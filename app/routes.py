688432aba9f34a57b086385aeeeecbe33c173d27
Mike Bromberek <mikebromberek@gmail.com>
2021-02-20 04:43:50 -0600

    Added profile page and profile image for users and their posts

--- app/routes.py
+++ app/routes.py
@@ -60,3 +60,13 @@ def register():
         flask.flash('Congratulations, you are now a registered user!')
         return flask.redirect(flask.url_for('login'))
     return flask.render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return flask.render_template('user.html', user=user, posts=posts)
