cbd483781b3358e66ceab7c27345ab381c56f788
Mike Bromberek <mikebromberek@gmail.com>
2021-02-21 12:25:46 -0600

    Added buttons for seeing older and newer posts

--- app/routes.py
+++ app/routes.py
@@ -29,7 +29,11 @@ def index():
 
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'], False)
-    site_code = flask.render_template('index.html', title='Home Page', form=form, posts=posts.items)
+    next_url = flask.url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = flask.url_for('inex', page=posts.prev_num) \
+        if posts.has_prev else None
+    site_code = flask.render_template('index.html', title='Home Page', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
     return site_code
 
 
@@ -38,7 +42,12 @@ def index():
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
-    return flask.render_template('index.html', title='Explore', posts=posts.items)
+    next_url = flask.url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = flask.url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+
+    return flask.render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET','POST'])
