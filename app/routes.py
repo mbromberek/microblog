7e80b41b11ae5a73137b4b3f040806aa94708943
Mike Bromberek <mikebromberek@gmail.com>
2021-02-20 18:32:52 -0600

    Users can follow and unfollow a user through web page now

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ import flask
 from flask import request
 from app import app
 from app import db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -76,7 +76,8 @@ def user(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
-    return flask.render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return flask.render_template('user.html', user=user, posts=posts, form=form)
 
 @app.route('/edit_profile', methods=['GET','POST'])
 @login_required
@@ -92,3 +93,41 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return flask.render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flask.flash('User {} not found.'.format(username))
+            return flask.redirect(flask.url_for('index'))
+        if user == current_user:
+            flask.flash('You cannot follow yourself!')
+            return flask.redirect(flask.url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flask.flash('You are following {}!'.format(username))
+        return flask.redirect(flask.url_for('user', username=username))
+    else:
+        return flask.redirect(flask.url_for('index'))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flask.flash('User {} not found.'.format(username))
+            return flask.redirect(flask.url_for('index'))
+        if user == current_user:
+            flask.flash('You cannot unfollow yourself!')
+            return flask.redirect(flask.url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flask.flash('You are not following{}.'.format(username))
+        return flask.redirect(flask.url_for('user', username=username))
+    else:
+        return flask.redirect(flask.url_for('index'))
