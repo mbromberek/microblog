e4354280d8e8c6e22e91e7df023dc8290efa8b06
Mike Bromberek <mikebromberek@gmail.com>
2021-03-07 15:32:25 -0600

    Notification count will update dynamically without the user reloading the page

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 import jwt
+import json
+from time import time
 
 # Custom Classes
 from app import db, login
@@ -75,6 +77,7 @@ class User(UserMixin, db.Model):
     messages_sent = db.relationship('Message', foreign_keys = 'Message.sender_id', backref='author', lazy='dynamic')
     messages_received = db.relationship('Message', foreign_keys='Message.recipient_id', backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -126,6 +129,12 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter( \
             Message.timestamp > last_read_time).count()
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
@@ -148,3 +157,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
