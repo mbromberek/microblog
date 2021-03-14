846cfdb63e7a1ac78e97833fd5d0ca986d067e72
Mike Bromberek <mikebromberek@gmail.com>
2021-03-14 09:45:50 -0500

    APIs setup for getting details about a user by ID or list of users

--- app/api/users.py
+++ app/api/users.py
@@ -1,12 +1,23 @@
+# 3rd Party classes
+from flask import jsonify, request, url_for, abort
+
+# Custom Classes
+from app import db
+from app.models import User
 from app.api import bp
+# from app.api.auth import token_auth
+from app.api.errors import bad_request
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
-    pass
+    return jsonify(User.query.get_or_404(id).to_dict())
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    pass
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
+    return jsonify(data)
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
 def get_followers(id):
