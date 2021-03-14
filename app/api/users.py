57da144d8ee36233bbfb80a92a42083cdc086239
Mike Bromberek <mikebromberek@gmail.com>
2021-03-14 10:15:37 -0500

    Setup APIs to return follows and followers for a user and create_user

--- app/api/users.py
+++ app/api/users.py
@@ -21,15 +21,35 @@ def get_users():
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
 def get_followers(id):
-    pass
+    user = User.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = User.to_collection_dict(user.followers, page, per_page, 'api.get_followers', id=id)
+    return jsonify(data)
 
 @bp.route('/users/<int:id>/followed', methods=['GET'])
 def get_followed(id):
-    pass
+    user = User.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = User.to_collection_dict(user.followed, page, per_page, 'api.get_followed', id=id)
+    return jsonify(data)
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email, and password fields')
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request('please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
