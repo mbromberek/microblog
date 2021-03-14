11159fff83125ed010c8eebd97cfa00fdefdfda8
Mike Bromberek <mikebromberek@gmail.com>
2021-03-14 13:48:37 -0500

    Fix bug for error_response

--- app/api/errors.py
+++ app/api/errors.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from werkzeug.http import HTTP_STATUS_CODES
 
 def error_response(status_code, message=None):
-    payload = {'error': HTTP_STATUS_cODES.get(status_code, 'Unknown error')}
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
     if message:
         payload['message'] = message
     response = jsonify(payload)
